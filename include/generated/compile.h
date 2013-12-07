e2799fd1265c9fb65405b5e1469cc7e397de3498
javilonas <admin@lonasdigital.com>
2013-12-07 10:41:54 +0100

    Update files

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
 /* This file is auto generated, version Stock */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#Stock SMP PREEMPT Sat Dec 7 06:05:44 CET 2013"
+#define UTS_VERSION "#Stock SMP PREEMPT Sat Dec 7 10:21:39 CET 2013"
 #define LINUX_COMPILE_BY "lonas"
 #define LINUX_COMPILE_HOST "lonas-linux"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
