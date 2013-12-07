a1dd6261d45fdd314b62bfcd169a99610f98aec8
javilonas <admin@lonasdigital.com>
2013-12-07 11:18:51 +0100

    Update files

--- ramdisk-samsung/res/ext/bootanimation.sh
+++ ramdisk-samsung/res/ext/bootanimation.sh
@@ -6,5 +6,6 @@
 if /sbin/busybox [ -f /data/local/bootanimation.zip ] || /sbin/busybox [ -f /system/media/bootanimation.zip ]; then
         /sbin/bootanimation &
 else
-        /sbin/samsungani &
+        /system/bin/bootanimation &
 fi
+
